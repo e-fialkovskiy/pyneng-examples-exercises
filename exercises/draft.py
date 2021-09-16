50934b787e578ae9666faee39c82a43465b3ebcc
e-fialkovskiy <e.fialkovskiy@yandex.ru>
2021-09-15 23:28:59 -0700

    Сделаны задания 4.3 4.4

---
+++ exercises/draft.py
@@ -0,0 +1,33 @@
+string = "interface FastEthernet0/1"
+#print(string[-1])
+
+# ip = "192.168.1.50"
+# print(ip.split('.'))
+
+# first_name = "Evgeniy"
+last_name = "Fialkovskiy"
+#
+# print(f"{first_name.upper()} {last_name.upper()}")
+
+# print(list(last_name))
+# #print(type(last_name))
+# print(last_name[::2])
+
+# vlans = ['10', '15', '20', '30', '100-200']
+# #vlans.reverse()
+# print(vlans[::-1])
+
+# interfaces = [['FastEthernet0/0', '15.0.15.1', 'YES', 'manual', 'up', 'up'],
+#               ['FastEthernet0/1', '10.0.1.1', 'YES', 'manual', 'up', 'up'],
+#               ['FastEthernet0/2', '10.0.2.1', 'YES', 'manual', 'up', 'down']]
+# print(interfaces[1][1])
+
+# names = ['John', 'Michael', 'Antony']
+#
+# print(sorted(names))
+# print(names)
+
+vlans = ['10', '20', '30', '100-200']
+vlans2 = ['300', '400', '500']
+vlans.extend(vlans2)
+print(vlans)
